f51c5770923cfbb29be337f79b725c958de776bf
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-10-23 11:55:45 +0200

    feat: add plausible script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+    app: {
+        head: {
+            script: [
+                {
+                    defer: true,
+                    src: 'https://plausible.io/js/plausible.js',
+                    'data-domain': 'nuxt-partytown.vercel.app'
+                }
+            ]
+        }
+    }
 })
