f9f949e4bba93bd5ec5e8ee68111993dedc98ba9
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-10-23 12:03:59 +0200

    feat: add partytown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,21 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            script: [
-                {
-                    defer: true,
-                    src: 'https://plausible.io/js/plausible.js',
-                    'data-domain': 'nuxt-partytown.vercel.app'
-                }
-            ]
-        }
-    }
-})
+  modules: ["@nuxtjs/partytown"],
+  partytown: {
+    forward: ["$plausible", "$plausible.push"],
+  },
+  app: {
+    head: {
+      script: [
+        { children: "window.$plausible = [];" },
+        // Update this
+        {
+          src: "https://plausible.io/js/script.js",
+          defer: true,
+          type: "text/partytown",
+          "data-domain": "nuxt-partytown.vercel.app",
+        },
+      ],
+    },
+  },
+});
